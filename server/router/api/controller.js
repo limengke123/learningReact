f5575bf7dc4ca9ba1f61687a956026ca6e7d8300
limengke <752140235@qq.com>
2017-09-02 13:52:57 +0800

    提交

--- server/router/api/controller.js
+++ server/router/api/controller.js
@@ -1,12 +1,14 @@
 const request = require('request-promise');
 const iconv = require('iconv-lite');
 const test = async function(ctx){
-    const url = 'http://www.xieet.com/shaonv/';
-    const body = await request.get(url);
+    const url = 'http://www.xieet.com/meinv/230.html';
+    const body = await request.get({url,gzip:true});
     ctx.response.type= "text";
+    const body1 = iconv.decode(body,'gb2312');
     //console.log(iconv.decode(body,'utf-8'));
+    //console.log(iconv.decode(body,'gb2312'));
     //console.log(body);
-    ctx.body = body
+    ctx.body = body1
 };
 
 const random = async function(ctx){
