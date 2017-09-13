8684949fee9a58690c9ed43641ccfe10a245465b
limengke <752140235@qq.com>
2017-09-13 17:46:29 +0800

    搞不清楚的问题

--- src/page/manger/mangerPage.js
+++ src/page/manger/mangerPage.js
@@ -1,37 +1,30 @@
 'use strict';
 
 import React from 'react';
-import {Form,Input} from 'antd';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
 const FormItem = Form.item;
 import './manger.less'
-class MangerPage extends React.Component{
-    constructor(){
+class MangerPage extends React.Component {
+    constructor() {
         super(...arguments);
-    }
-    render(){
-        const formItemLayout = {
-            labelCol: { span: 6 },
-            wrapperCol: { span: 14 },
+        this.state = {
+            expand: false
         };
-        return(
+    }
+
+
+    render() {
+        const formItemLayout = {
+            labelCol: {span: 6},
+            wrapperCol: {span: 14},
+        };
+        return (
             <div className="manger-page">
-                {/*<Form>
-                    <FormItem
-                        {...formItemLayout}
-                        label="username">
-                        {getFieldDecorator('username', {
-                            rules: [{
-                                required: true,
-                                message: 'Please input your name',
-                            }],
-                        })(
-                            <Input placeholder="Please input your name" />
-                        )}
-                    </FormItem>
-                </Form>*/}
             </div>
         )
     }
 }
+
+
 
 export default MangerPage;
