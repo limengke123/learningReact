e3d92b072ca5e7a4b585b4380858630d02b283a1
limengke <752140235@qq.com>
2017-09-23 12:01:39 +0800

    解决dytt乱码问题

--- server/controller/spider/dytt.js
+++ server/controller/spider/dytt.js
@@ -8,10 +8,11 @@ const iconv = require('iconv-lite');
 
 const dytt = async (ctx)=>{
     const url = "http://www.dytt8.net/html/gndy/dyzz/list_23_1.html";
-    const body = await request(url);
+    const buffer = await request({uri:url,encoding: null});
+    const body = iconv.decode(buffer,'gbk')
     const $ = cheerio.load(body);
     //ctx.response.type = 'text';
-    ctx.response.setEncode('gbk');
+    //ctx.response.setEncode('gbk');
     ctx.body = $(".co_content8").find("table").eq(0).find('.ulink').text()
 };
 
